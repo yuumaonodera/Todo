41bb065fd85759d042624cfeacc13a33280950f4
ユウマオノデラ <yuuma.30333@icloud.com>
2022-06-01 19:43:33 +0900

    エラー

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Models\Todo;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 
--- database/migrations/2022_06_01_100747_create_todos_table.php
+++ database/migrations/2022_06_01_100747_create_todos_table.php
@@ -16,8 +16,8 @@ class UserTodoTable extends Migration
         Schema::create('todo_list', function (Blueprint $table){
             $table -> id();
             $table -> string('content', 191);
-            $table -> time_stamps('created_at')->nullable();
-            $table -> time_stamps('updated_at')->nullable();
+            $table -> timestamp('created_at')->nullable();
+            $table -> timestamp('updated_at')->nullable();
         });
     }
 
